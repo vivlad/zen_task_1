e9a5a13b8d0593667f8afb026b8be5afeb60fdc9
gvv <0x142857ff@gmail.com>
2018-01-17 17:57:50 +0200

    added random color for items

--- src/Content.js
+++ src/Content.js
@@ -60,7 +60,7 @@ class Entries extends Component {
                 {
                     this.props.entries.map((item, idx) => {
                         return(
-                            <li key={idx}>
+                            <li key={idx} style={{color: '#'+Math.random().toString(16).slice(2, 8).toUpperCase()}}>
                                 {item.text}
                                 <span data-itemid={idx} className="entrie-remove" onClick={this.removeEntry}>x</span>
                             </li>
